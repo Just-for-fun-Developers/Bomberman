aa92f317fe63fc2bf4f6ffbc4d1d3d6dcbb8fbe2
kent.yucraquispe <kent.yucraquispe@smarkets.com>
2023-10-18 09:29:29 +0100

    Add button and signin with google

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import PlayScene from "./scenes/PlayScene";
 import PreloadScene from "./scenes/PreloadScene";
+import jwt_decode from "jwt-decode";
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -17,3 +18,14 @@ const config: Phaser.Types.Core.GameConfig = {
 };
 
 new Phaser.Game(config);
+
+function handleCredentialResponse(body: any) {
+  const decoded: any = jwt_decode(body["credential"]);
+  const name = decoded["name"];
+  const userNameElement = document.getElementById("userName");
+  if (userNameElement) {
+    userNameElement.textContent = `Hello, ${name}!`;
+  }
+}
+
+(window as any).handleCredentialResponse = handleCredentialResponse;
